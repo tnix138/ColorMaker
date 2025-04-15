e2f396d5c0c033005585a98e0d5add2ed7d8e980
tohid rajabali <tohidrajabali1380@gmail.com>
2025-04-15 17:06:48 +0330

    Remove AppShell, update project settings, change app icon
    Removed AppShell.xaml and AppShell.xaml.cs files, including all class definitions and initializations. Updated ApplicationId in ColorMaker.csproj to com.dana.colormaker. Added new PropertyGroup for Release|net9.0-android|AnyCPU configuration with AndroidPackageFormat set to apk. Changed MauiIcon from SVG to JPEG in ColorMaker.csproj. Removed Theme attribute from Activity in MainActivity.cs.

--- ColorMaker/Platforms/Android/MainActivity.cs
+++ ColorMaker/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace ColorMaker.Platforms.Android
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity( MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
